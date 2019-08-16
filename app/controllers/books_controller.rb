7ff57d036e33f50b85c717a68df1ce03ea687688
坂元丈将 <takemasa@sakamotojoushou-no-MacBook-Pro.local>
2019-08-16 22:00:52 +0900

    4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  
+  def top
+
+
+  end
+
+  def new
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def create
+	book = Book.new(book_params)
+    if book.save
+     	flash[:notice] = "successfully"
+        redirect_to book_path(book.id)
+        
+	end
+  end
+
+    def index
+    	@books = Book.all
+    end
+    
+    def show
+    	 @book = Book.find(params[:id])
+    end
+
+    def edit
+         @book = Book.find(params[:id])
+    end
+
+    def update
+        book = Book.find(params[:id])
+        book.update(book_params)
+        redirect_to book_path(book.id)
+    end
+
+    def destroy
+    	book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_new_path
+    end
+
+  private
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+
+end
